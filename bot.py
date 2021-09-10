7f056811c7d5f6db83d7d42af38421b595d91adb
BBanDD <71388284+BBanDD@users.noreply.github.com>
2021-09-10 11:41:26 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,4 +18,4 @@ async def on_ready():
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 
-client.run(os.environ['token'])
+client.run(os.environ['ODg1NjkxODIyOTU1OTY2NTI0.YTquyQ.Ec6NO9jHMjJzJzgdsmhbMp47oes'])
